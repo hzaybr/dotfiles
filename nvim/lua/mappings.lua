b7dea487f65508e9fe55714aff82bbc396b4cb2d
hzaybr <ahyebear@gmail.com>
2026-04-02 17:57:42 +0800

    refactor: colocate plugin keymaps with their plugin definitions
    - Move copilot keymaps from mappings.lua into copilot plugin config
    - Extract gitsigns on_attach keymaps to configs/gitsigns.lua

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,14 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
 
--- Copilot mappings
-map("i", "<C-g>", function()
-  vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
-end, { desc = "Copilot Accept All" })
-map("i", "<C-y>", "<Plug>(copilot-accept-line)", { desc = "Copilot Accept Line" })
-map("i", "<C-]>", "<Plug>(copilot-next)")
-map("i", "<C-x>", "<Plug>(copilot-dismiss)")
-
 -- Override leader-e to toggle instead of focus
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
 
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -19,6 +19,14 @@ return {
       vim.g.copilot_enable = true
       vim.g.copilot_no_tab_map = true
       vim.g.copilot_assume_mapped = true
+
+      local map = vim.keymap.set
+      map("i", "<C-g>", function()
+        vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+      end, { desc = "Copilot Accept All" })
+      map("i", "<C-y>", "<Plug>(copilot-accept-line)", { desc = "Copilot Accept Line" })
+      map("i", "<C-]>", "<Plug>(copilot-next)")
+      map("i", "<C-x>", "<Plug>(copilot-dismiss)")
     end,
   },
 
@@ -36,54 +44,7 @@ return {
 
   {
     "lewis6991/gitsigns.nvim",
-    opts = {
-      on_attach = function(bufnr)
-        local gs = require "gitsigns"
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map("n", "]c", function()
-          if vim.wo.diff then
-            vim.cmd.normal { "]c", bang = true }
-          else
-            gs.nav_hunk "next"
-          end
-        end)
-        map("n", "[c", function()
-          if vim.wo.diff then
-            vim.cmd.normal { "[c", bang = true }
-          else
-            gs.nav_hunk "prev"
-          end
-        end)
-
-        map("n", "<leader>hs", gs.stage_hunk)
-        map("n", "<leader>hr", gs.reset_hunk)
-        map("v", "<leader>hs", function()
-          gs.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
-        end)
-        map("v", "<leader>hr", function()
-          gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
-        end)
-        map("n", "<leader>hS", gs.stage_buffer)
-        map("n", "<leader>hR", gs.reset_buffer)
-        map("n", "<leader>hp", gs.preview_hunk)
-        map("n", "<leader>hi", gs.preview_hunk_inline)
-        map("n", "<leader>hb", function()
-          gs.blame_line { full = true }
-        end)
-        map("n", "<leader>hd", gs.diffthis)
-        map("n", "<leader>hD", function()
-          gs.diffthis "~"
-        end)
-        map("n", "<leader>tb", gs.toggle_current_line_blame)
-        map("n", "<leader>tw", gs.toggle_word_diff)
-        map({ "o", "x" }, "ih", gs.select_hunk)
-      end,
-    },
+    opts = require "configs.gitsigns",
   },
 
   -- test new blink
